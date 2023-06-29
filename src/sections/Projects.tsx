a12ba9e13d40771350a76acff69d93bfb5d4fbf5
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-29 19:39:02 -0300

    feat: rendering component project card and implementing logic to know the size of the fabric

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 
-import { staggerContainer } from '@/utils/motion';
 import { TitleText, TypingText } from '@/components';
+import ProjectCard from '@/components/ProjectCard';
 
-import styles from '../styles';
+import { projects } from '@/constants';
+import { staggerContainer } from '@/utils/motion';
+
+import styles from '@/styles';
 
 const Projects: React.FC = () => {
+    const [active, setActive] = useState<string>('world-2');
+    const [isLgScreen, setIsLgScreen] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLgScreen(window.innerWidth >= 1024);
+        };
+
+        if (typeof window !== 'undefined') {
+            setIsLgScreen(window.innerWidth >= 1024);
+            window.addEventListener('resize', handleResize);
+
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
+        }
+    }, []);
 
     return (
-        <section className={`${styles.paddings}`} id="projetos">
+        <section className='sm:p-16 xs:p-8 px-6 py-12 mt-24 border' id="projetos" >
             <motion.div
                 variants={staggerContainer(0.1, 0.1)}
                 initial="hidden"
@@ -18,8 +39,23 @@ const Projects: React.FC = () => {
             >
                 <TypingText title="| Desenvolvendo Soluções Criativas" textStyles="text-center" />
                 <TitleText title="Projetos" textStyles="text-center" />
-                <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5 ">
-                {/* projects */}
+                <div className="mt-[20px] flex lg:flex-row flex-col gap-5  sm:h-[700px] md:h-[780px] lg:h-[520px] h-[780px]">
+                    {projects.map((world, index) => (
+                        <ProjectCard
+                            key={world.id}
+                            {...world}
+                            id={world.id}
+                            mobUrl={world.mobUrl}
+                            deskUrl={world.deskUrl}
+                            title={world.title}
+                            index={index}
+                            active={active}
+                            handleClick={setActive}
+                            isLgScreen={isLgScreen}
+                            repositorio={world.repositorio}
+                            deploy={world.deploy}
+                        />
+                    ))}
                 </div>
             </motion.div>
         </section>
