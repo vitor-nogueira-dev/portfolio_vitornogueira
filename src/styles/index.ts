f49f92392b7b54744a39c29534e21f7ac9bba816
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-29 19:36:13 -0300

    fix: removing white lines

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,18 +3,15 @@ import { IStyles } from "@/interfaces/IStyles";
 const styles: IStyles = {
     innerWidth: '2xl:max-w-[1280px] w-full',
     interWidth: 'lg:w-[80%] w-[100%]',
-
     paddings: 'sm:p-16 xs:p-8 px-6 py-12',
     yPaddings: 'sm:py-16 xs:py-8 py-12',
     xPaddings: 'sm:px-16 px-6',
     topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
     bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
-
     flexCenter: 'flex justify-center items-center',
     flexStart: 'flex justify-start items-start',
     flexEnd: 'flex justify-end',
     navPadding: 'pt-[98px]',
-
     heroHeading:
         'font-bold lg:text-[60px] md:text-[80px] sm:text-[40px] text-[44px] lg:leading-[157px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] text-white',
     heroDText:
