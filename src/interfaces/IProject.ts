448f40d2ac89de5ebeed119fdda74f60b0a19c87
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-29 11:40:33 -0300

    feat: raising interface project

---
+++ src/interfaces/IProject.ts
@@ -0,0 +1,9 @@
+export interface IProject {
+    id: string;
+    mobUrl: string;
+    deskUrl: string;
+    title: string;
+    stacks: string[];
+    repositorio: string;
+    deploy: string;
+  }
