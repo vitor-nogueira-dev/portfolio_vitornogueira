9fb23b300fc0daa531d914fafbaf79dcb0d7a46e
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-29 11:20:04 -0300

    feat: raising interface styles

---
+++ src/interfaces/IStyles.ts
@@ -0,0 +1,15 @@
+export interface IStyles {
+    innerWidth: string;
+    interWidth: string;
+    paddings: string;
+    yPaddings: string;
+    xPaddings: string;
+    topPaddings: string;
+    bottomPaddings: string;
+    flexCenter: string;
+    flexStart: string;
+    flexEnd: string;
+    navPadding: string;
+    heroHeading: string;
+    heroDText: string;
+}
