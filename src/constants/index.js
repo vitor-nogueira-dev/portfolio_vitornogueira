ebd66b48cb4059b3f4ba431ea166436a3a91e7f4
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-29 10:53:09 -0300

    feat: adding data constants

---
+++ src/constants/index.js
@@ -0,0 +1,333 @@
+export const exploreWorlds = [
+  {
+    id: 'netflix',
+    mobUrl: '/mobNetflix.png',
+    deskUrl: '/deskNetflix.png',
+    title: 'Netflix',
+    stacks: [
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    ],
+    repositorio: 'https://github.com/vitor-nogueira-dev/netflix',
+    deploy: 'https://netflix-vn.netlify.app/',
+  },
+  {
+    id: 'fitclub',
+    mobUrl: '/mobFitClub.png',
+    deskUrl: '/deskFitClub.png',
+    title: 'FitClub',
+    stacks: [
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    ],
+    repositorio: 'https://github.com/vitor-nogueira-dev/gym-fitclub',
+    deploy: 'https://fitclub-vn.netlify.app/',
+  },
+  {
+    id: 'weekaway',
+    mobUrl: '/mobWeekAway.png',
+    deskUrl: '/deskWeekAway.png',
+    title: 'WeekAway',
+    stacks: [
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+    ],
+    repositorio: 'https://github.com/vitor-nogueira-dev/weekaway',
+    deploy: 'https://weekaway-vn.netlify.app/',
+  },
+  {
+    id: 'Wallet',
+    mobUrl: '/mobWallet.png',
+    deskUrl: '/deskWallet.png',
+    title: 'Wallet',
+    stacks: [
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+      '/styled-components.png',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    ],
+    repositorio: 'https://github.com/vitor-nogueira-dev/trybe-wallet',
+    deploy: 'https://trybe-wallet-vn.netlify.app/',
+  },
+  {
+    id: 'Tunes',
+    mobUrl: '/mobTunes.png',
+    deskUrl: '/deskTunes.png',
+    title: 'Tunes',
+    stacks: [
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+      '/styled-components.png',
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    ],
+    repositorio: 'https://github.com/vitor-nogueira-dev/trybe-tunes',
+    deploy: 'https://trybetunes-vn.netlify.app/',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const softSkills = [
+  {
+    title: 'Habilidade em Resolver Problemas',
+    subtitle:
+      'Enfrento desafios com uma abordagem analítica e buscando soluções eficientes.',
+    imgUrl: '/puzzle.png',
+  },
+  {
+    title: 'Capacidade de Trabalhar em Equipe',
+    subtitle:
+      'Colaboro de forma efetiva, ouvindo os membros da equipe e contribuindo para alcançar objetivos comuns.',
+    imgUrl: '/networking.png',
+  },
+  {
+    title: 'Atenção aos Detalhes para Interfaces Bem Elaboradas',
+    subtitle:
+      'Tenho um olhar minucioso para garantir que as interfaces sejam visualmente atraentes e funcionais.',
+    imgUrl: '/loupe.png',
+  },
+  {
+    title: 'Orientado(a) a Processos e Resultados',
+    subtitle:
+      'Sigo metodologias e boas práticas para entregar projetos bem-sucedidos dentro do prazo e do orçamento.',
+    imgUrl: '/bar-chart.png',
+  },
+  {
+    title: 'Pensamento Criativo para Soluções Inovadoras',
+    subtitle:
+      'Busco constantemente maneiras criativas e inovadoras de abordar problemas e encontrar soluções.',
+    imgUrl: '/idea.png',
+  },
+  {
+    title: 'Flexibilidade e Adaptação a Diferentes Tecnologias e Requisitos',
+    subtitle:
+      'Sou capaz de me adaptar a novas tecnologias e requisitos, buscando sempre aprender e evoluir.',
+    imgUrl: '/mobile.png',
+  },
+];
+
+export const experiencies = [
+  {
+    imgUrl: '/cake.png',
+    title: 'Doces da Hora',
+    office: 'Sócio Proprietário',
+    period: '2020 - 2022',
+    activities: [
+      '- Gerenciamento de todas as operações do negócio, incluindo a produção de doces e sobremesas, controle de estoque, vendas e atendimento ao cliente.',
+      '- Estratégias de marketing e publicidade para aumentar o alcance e a visibilidade da empresa.',
+    ],
+  },
+  {
+    imgUrl: '/equipe.png',
+    title: 'Espaço Avenida e restaurante',
+    office: 'Gerente Geral',
+    period: '2018 - 2020',
+    activities: [
+      '- Responsável por uma equipe de aproximadamente 14 colaboradores, com foco na melhoria contínua do desempenho e engajamento da equipe.',
+      '- Responsável pela gestão de pessoas e rotinas de RH, incluindo recrutamento, treinamento e desenvolvimento de equipe.',
+      '- Controle de qualidade das operações, assegurando o cumprimento das normas e padrões de segurança.',
+    ],
+  },
+  {
+    imgUrl: '/sales.png',
+    title: 'Autônomo',
+    office: 'Vendedor externo',
+    period: '2010 - 2014',
+    activities: [
+      '- Vendas externas de produtos alimentícios, incluindo doces e salgados.',
+      '- Atendimento ao cliente, incluindo negociação de preços e condições de pagamento.',
+    ],
+  },
+];
+
+export const socials = [
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/vitor-nogueira-dev/',
+  },
+  {
+    name: 'github',
+    url: '/github.svg',
+    link: 'https://github.com/vitor-nogueira-dev',
+  },
+  {
+    name: 'whatsapp',
+    url: '/whatsapp.svg',
+    link: 'https://wa.me/5531998193412',
+  },
+];
+
+export const stacks = [
+  {
+    name: 'HTML',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    content:
+      'Linguagem de marcação utilizada para estruturar o conteúdo de páginas web.',
+  },
+  {
+    name: 'CSS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+    content:
+      'Linguagem de estilo utilizada para definir a apresentação e o layout de páginas web.',
+  },
+  {
+    name: 'Javascript',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    content:
+      'Linguagem de programação utilizada para adicionar interatividade e comportamento às páginas web.',
+  },
+  {
+    name: 'Typescript',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    content:
+      'Superset do JavaScript que adiciona recursos de tipagem estática à linguagem.',
+  },
+  {
+    name: 'React JS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    content:
+      'Biblioteca JavaScript utilizada para construir interfaces de usuário (UI) reativas e componentizadas.',
+  },
+  {
+    name: 'Redux',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    content:
+      'Biblioteca JavaScript utilizada para gerenciar o estado global de uma aplicação React.',
+  },
+  {
+    name: 'Next JS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+    content:
+      'Framework React utilizado para desenvolvimento de aplicações web do lado do servidor (SSR) e renderização estática (SSG).',
+  },
+  {
+    name: 'Node JS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    content:
+      'Ambiente de execução JavaScript do lado do servidor, baseado no motor V8 do Google Chrome.',
+  },
+  {
+    name: 'Express JS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+    content: 'Framework web rápido e flexível para aplicativos Node.js.',
+  },
+  {
+    name: 'Sequelize',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg',
+    content:
+      'ORM (Object-Relational Mapping) em JavaScript para bancos de dados relacionais.',
+  },
+  {
+    name: 'MySQL',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+    content:
+      'Sistema de gerenciamento de banco de dados relacional (RDBMS) popular.',
+  },
+  {
+    name: 'Docker',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    content:
+      'Plataforma de virtualização que permite empacotar e distribuir aplicativos em contêineres.',
+  },
+  {
+    name: 'Git',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    content:
+      'Sistema de controle de versão distribuído usado para rastrear mudanças no código-fonte durante o desenvolvimento de software.',
+  },
+  {
+    name: 'Github',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+    content:
+      'Plataforma de hospedagem de código-fonte e colaboração que usa o Git.',
+  },
+  {
+    name: 'Styled Components',
+    url: '/styled-components.png',
+    content:
+      'Biblioteca para estilização em componentes React, utilizando CSS-in-JS.',
+  },
+  {
+    name: 'Bootstrap',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg',
+    content:
+      'Framework front-end popular para desenvolvimento rápido e responsivo de sites e aplicativos.',
+  },
+  {
+    name: 'Tailwind CSS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+    content:
+      'Framework CSS utilitário que fornece classes pré-definidas para estilizar componentes.',
+  },
+  {
+    name: 'Material UI',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg',
+    content:
+      'Biblioteca de componentes React que implementa as diretrizes de design do Material Design.',
+  },
+  {
+    name: 'Figma',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
+    content:
+      'Ferramenta de design de interface de usuário (UI) e prototipagem colaborativa baseada na web.',
+  },
+  {
+    name: 'Mocha',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg',
+    content:
+      'Framework de teste JavaScript executado no Node.js e no navegador.',
+  },
+  // {
+  //   name: 'Chai',
+  //   url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/chai/chai-original.svg',
+  //   content: 'Biblioteca de asserção para testes em JavaScript.',
+  // },
+  // {
+  //   name: 'Sinon',
+  //   url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sinonjs/sinonjs-original.svg',
+  //   content: 'Biblioteca de testes em JavaScript que fornece recursos de criação de espiões, stubs e mocks.',
+  // },
+  {
+    name: 'Jest',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    content:
+      'Framework de testes em JavaScript amplamente utilizado para testar aplicações e componentes React.',
+  },
+  {
+    name: 'React Testing Library',
+    url: '/testing-library.png',
+    content:
+      'Biblioteca de testes em JavaScript voltada para testar componentes React, seguindo princípios de teste de usuário final.',
+  },
+];
+
+export const feedbacks = [
+  {
+    name: 'Pablo',
+    cargo: 'Developer Front-end',
+    content:
+      'Vitor é extremamente dedicado ao seu comprometimento em todos os aspectos!!! Uma pessoa tranquila, sincera com quem consegui o início de uma amizade, do qual desejo que continue crescendo e torna-se enorme. E digo sem medo, que não seja somente no âmbito estudantil e profissional.Escrevo isso porque no dia a dia Trybe de estudo, é sensacional demais ver o crescimento rápido de aprendizado desenvolvimento web dele e ele também estar sempre disposto a ajudar e ensinar o próximo. Desenvolver projetos sempre com algo a mais, sempre dando dicas sobre caso a caso. Sempre que pode está me ajudando 😉. Vitor no momento é um Front-end Jr exemplo, com foco em virar um desenvovedor Full-Stack de primeira linha!!',
+  },
+  {
+    name: 'José Alves',
+    cargo: 'Developer Back-end',
+    content:
+      'Estou extremamente impressionado com a capacidade e determinação de Vitor na área de tecnologia. Apesar de ter começado do zero, ele conseguiu adquirir, em poucos meses, um notável nível de conhecimento e profissionalismo em suas atividades. Além disso, é admirável como ele se dedica e mantém focado em seus objetivos, mostrando-se um profissional altamente talentoso e dedicado. Vitor trabalha muito bem em equipe, contribuindo para um ambiente de trabalho colaborativo e produtivo. Além disso, ele possui uma excelente didática, tornando-se capaz de transmitir seus conhecimentos de forma clara e compreensível para todos os membros da equipe. Com essas habilidades adicionais, tenho certeza de que o Vitor seria um grande trunfo para qualquer equipe de trabalho. Portanto, recomendo fortemente o Vitor para qualquer empresa que busque um colaborador com alto potencial e comprometimento.',
+  },
+  {
+    name: 'Wallace Ferreia',
+    cargo: 'Developer Back-end',
+    content:
+      'Recomendo Vitor Nogueira para qualquer oportunidade na área de desenvolvimento de software. Ele é um desenvolvedor dedicado e está sempre em busca de aprimorar suas habilidades técnicas e interpessoais. Vitor tem uma abordagem criativa para resolver problemas complexos e é capaz de trabalhar bem em equipe. Além disso, ele é um comunicador claro e eficaz, capaz de explicar conceitos técnicos de forma clara e concisa. Estou confiante de que Vitor será um ativo valioso para qualquer equipe de desenvolvimento e recomendo-o sem hesitação.',
+  },
+];
