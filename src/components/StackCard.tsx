d5daa986921957a942fdfb7d669fcf94131a5ad0
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-29 20:13:33 -0300

    feat: raising component stack card

---
+++ src/components/StackCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../styles';
+import { IStackCardProps } from '@/interfaces/IStackCardProps';
+import Image from 'next/image';
+
+export const StackCard: React.FC<IStackCardProps> = ({ stack }) => (
+  <div
+    className={`${styles.flexCenter} w-[50px] h-[50px] rounded-[24px] glassmorphism mb-[16px] m-1 lg:w-[60px] lg:h-[40px]`}
+  >
+    <Image
+      src={stack}
+      className="object-contain"
+      alt='stack'
+      width={25}
+      height={25}
+    />
+  </div>
+);
