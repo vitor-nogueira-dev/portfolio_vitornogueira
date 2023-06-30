50dbdd9770a2eac64e2263577c9b9c4544e82d7e
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-30 11:20:26 -0300

    feat: adding imports hard skill card barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,13 @@ import Navbar from "./NavBar";
 import { TitleText, TypingText } from './CustomTexts'
 import StackCard from "./StackCard";
 import ProjectCard from "./ProjectCard";
+import HardSkillsCard from "./HardSkillsCard";
 
-export { Navbar, TitleText, TypingText, StackCard, ProjectCard };
+export {
+    Navbar,
+    TitleText,
+    TypingText,
+    StackCard,
+    ProjectCard,
+    HardSkillsCard,
+};
