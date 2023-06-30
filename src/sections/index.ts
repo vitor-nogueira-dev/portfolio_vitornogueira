df41b64169ac94188828826dcc1e50c3c6217526
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-30 11:26:44 -0300

    feat: adding import section softskills barrel

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -2,5 +2,12 @@ import About from "./About";
 import Hero from "./Hero";
 import Projects from "./Projects";
 import HardSkills from "./HarkSkills";
+import SoftSkills from "./SoftSkills";
 
-export { About, Hero, Projects, HardSkills };
+export {
+    About,
+    Hero,
+    Projects,
+    HardSkills,
+    SoftSkills,
+};
