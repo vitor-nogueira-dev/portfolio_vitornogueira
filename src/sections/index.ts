e4a4521cc644f7d517b686fd3c2d7c4fc82e5540
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-30 15:03:00 -0300

    feat: adding section footer to barrel

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -3,6 +3,7 @@ import Hero from "./Hero";
 import Projects from "./Projects";
 import HardSkills from "./HarkSkills";
 import SoftSkills from "./SoftSkills";
+import Footer from "./Footer";
 
 export {
     About,
@@ -10,4 +11,5 @@ export {
     Projects,
     HardSkills,
     SoftSkills,
+    Footer,
 };
