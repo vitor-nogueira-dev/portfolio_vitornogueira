bdc899afe06956740183090ceb528367ac156cef
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-30 15:23:38 -0300

    feat: creating social media card

---
+++ src/components/RedesSociaisCard.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react"
+
+import { IRedesSociaisCardProps } from "@/interfaces/IRedesSociaisCardProps"
+
+const RedesSociaisCard: React.FC<IRedesSociaisCardProps> = ({ link, name, url }) => {
+    return (
+        <a href={link} key={name} target="_blank" rel="noopener noreferrer" className="cursos-pointer test-svg">
+            <Image
+                src={url}
+                alt={name}
+                className="object-contain cursor-pointer"
+                width={30}
+                height={30}
+            />
+        </a>
+    )
+}
+
+export default RedesSociaisCard;
