afba857dd80653dd67dd2701fe29679d5b233ff3
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-30 11:18:04 -0300

    feat: creating component hard skill card

---
+++ src/components/HardSkillsCard.tsx
@@ -0,0 +1,34 @@
+
+import { IHardSkillsCardProps } from '@/interfaces/IHardSkillsCardProps';
+import styles from '@/styles';
+import Image from 'next/image';
+import React from 'react';
+import { Tooltip, Whisper, Button, ButtonToolbar } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+
+const HardSkillsCard: React.FC<IHardSkillsCardProps> = ({ name, url, content }) => (
+    <div className={`${styles.flexCenter} flex-row`}>
+        <ButtonToolbar>
+            <Whisper placement="bottom" controlId="control-id-hover" trigger="hover" speaker={
+                <Tooltip>
+                    {content}
+                </Tooltip>
+            }>
+                <div
+                    className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] glassmorphism mb-[16px] m-1`}
+                    title={name}
+                >
+                    <Image
+                        src={url}
+                        className="w-1/2 h-1/2 object-contain cursor-pointer"
+                        width={40}
+                        height={40}
+                        alt={name}
+                    />
+                </div>
+            </Whisper>
+        </ButtonToolbar>
+    </div>
+);
+
+export default HardSkillsCard;
