28479f9921b46a8078aa8561c5f00f52044b71b4
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-01 18:12:47 -0300

    fix: removing soft skills card borders

--- src/components/SoftSkillsCard.tsx
+++ src/components/SoftSkillsCard.tsx
@@ -6,7 +6,7 @@ import { ISoftSkillsCardProps } from '@/interfaces/ISoftSkillsCardProps';
 import styles from '../styles';
 
 const SoftSkillsCard: React.FC<ISoftSkillsCardProps> = ({ imgUrl, title, subtitle }) => (
-    <div className="cardSoftSkillsExterno lg:w-[210px] lg:h-[350px] w-[213px] h-[290px] border border-solid border-white border-tl border-br"  >
+    <div className="cardSoftSkillsExterno lg:w-[210px] lg:h-[350px] w-[213px] h-[290px]"  >
         <div className='cardSoftSkillsInterno 2 p-4 flex flex-col justify-between items-center lg:w-[210px] lg:h-[350px] w-[213px] h-[290px]'>
             <div
                 className={`${styles.flexCenter} w-[50px] h-[50px] rounded-[24px] glassmorphism lg:w-[70px] lg:h-[70px]`}
@@ -19,10 +19,10 @@ const SoftSkillsCard: React.FC<ISoftSkillsCardProps> = ({ imgUrl, title, subtitl
                     height={50}
                 />
             </div>
-            <h1 className="mt-[15px] font-bold text-[14px] lg:text-[16px] leading-[25px] text-white text-center border">
+            <h1 className="mt-[15px] font-bold text-[14px] lg:text-[16px] leading-[25px] text-white text-center w-[180px] ">
                 {title}
             </h1>
-            <p className="flex-1 mt-[16px] font-normal text-[14px] lg:text-[16px] text-[#B0B0B0] leading-[25px] text-center">
+            <p className="flex-1 mt-[16px] font-normal text-[14px] lg:text-[16px] text-[#B0B0B0] text-left w-[170px]">
                 {subtitle}
             </p>
         </div>
