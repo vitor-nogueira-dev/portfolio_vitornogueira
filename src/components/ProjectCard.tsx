5124aa78c2ccd113871ffda9b810b791ec416b4e
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-01 18:13:23 -0300

    fix: changing project card component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,77 +1,26 @@
-import { motion, HTMLMotionProps, Variants } from 'framer-motion';
+import { Col } from 'react-bootstrap';
 
-import { fadeIn } from '@/utils/motion';
-import Image from 'next/image';
-import { ProjectCardProps } from '@/interfaces/IProjectCardProps';
+import { IProjectCardProps } from '@/interfaces/IProjectCardProps';
+
 import StackCard from './StackCard';
 
-const ProjectCard: React.FC<ProjectCardProps> = ({
-    id,
-    mobUrl,
-    deskUrl,
+const ProjectCard: React.FC<IProjectCardProps> = ({
     title,
-    index,
-    active,
-    handleClick,
     stacks,
-    isLgScreen,
-    repositorio,
-    deploy,
 }) => {
-    const fadeInVariants = fadeIn('right', 'spring', index * 0.5, 0.75);
-
+    
     return (
-        <motion.div
-            variants={fadeInVariants as Variants}
-            className={`relative ${active === id ? 'lg:flex-[2] flex-[4] lg:max-w-[250px]' : 'lg:flex-[0.5] flex-[2]'
-                } flex items-center justify-center transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer border rounded-2xl`}
-            onClick={() => handleClick(id)}
-        >
-            <Image
-                src={isLgScreen ? mobUrl : deskUrl}
-                alt="planet-04"
-                className={`${active !== id ? 'opacity-60' : ''} absolute w-full h-full object-cover rounded-[24px]`}
-                unoptimized={true}
-                width={337}
-                height={400}
-            />
-            {active !== id ? (
-                <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0] p-10">
-                    {title}
-                </h3>
-            ) : (
-                <div className="absolute bottom-0 p-4 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)]  rounded-[24px] boxShadow h-[100%] lg:h-[40%]">
-                    <div className="w-full flex justify-start">
-                        {stacks?.map((stack, index) => (
-                            <StackCard stack={stack} key={index} />
-                        ))}
-                    </div>
-                    <span>
-                        <a
-                            href={repositorio}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="font-medium text-[16px] leading-[20.16px] text-link-projects"
-                        >
-                            📁 Repositório
-                        </a>
-                    </span>
-                    <span>
-                        <a
-                            href={deploy}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="font-normal text-[16px] leading-[20.16px] text-link-projects"
-                        >
-                            💻 Deploy
-                        </a>
-                    </span>
-                    <h2 className=" mt-2 font-semibold sm:text-[22px] text-[24px] text-white">
-                        {title}
-                    </h2>
+        <Col md={4} lg={4} sm={12} className="m-1 mt-2 card-project h-[200px] lg:w-[300px] w-[300px]">
+            <div className="card-project-details">
+                <p className="text-[1.5em] font-bold text-center">{title}</p>
+                <div className="w-full flex justify-start mb-2">
+                    {stacks?.map((stack, index) => (
+                        <StackCard stack={stack} key={index} />
+                    ))}
                 </div>
-            )}
-        </motion.div>
+            </div>
+            <button className="card-project-button btn z-10">More info</button>
+        </Col>
     );
 };
 
