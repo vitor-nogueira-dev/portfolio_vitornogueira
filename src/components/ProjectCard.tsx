b374dd2761cbd480dd6ea73af57ce73fcfb45bc7
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-01 20:17:14 -0300

    feat: rendering modal component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,16 +1,20 @@
-import { Col } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 
 import { IProjectCardProps } from '@/interfaces/IProjectCardProps';
 
-import StackCard from './StackCard';
+import { StackCard } from '@/components';
+import { useState } from 'react';
+import { ModalComponent } from '@/components';
 
 const ProjectCard: React.FC<IProjectCardProps> = ({
     title,
     stacks,
+    mobUrl,
 }) => {
-    
+    const [modalShow, setModalShow] = useState(false);
+
     return (
-        <Col md={4} lg={4} sm={12} className="m-1 mt-2 card-project h-[200px] lg:w-[300px] w-[300px]">
+        <Col md={4} lg={4} sm={12} className="m-2 card-project h-[200px] lg:w-[300px] w-[300px]">
             <div className="card-project-details">
                 <p className="text-[1.5em] font-bold text-center">{title}</p>
                 <div className="w-full flex justify-start mb-2">
@@ -19,7 +23,13 @@ const ProjectCard: React.FC<IProjectCardProps> = ({
                     ))}
                 </div>
             </div>
-            <button className="card-project-button btn z-10">More info</button>
+            <ModalComponent
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                title={title}
+                mobUrl={mobUrl}
+            />
+            <button className="card-project-button btn z-10" onClick={() => setModalShow(true)}>visualizar projeto</button>
         </Col>
     );
 };
