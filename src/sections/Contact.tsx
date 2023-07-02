6556ad0fa35ca452dc86c3238ec5580404c6f00b
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-02 15:45:37 -0300

    feat: rendering risk div

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 
-import { Forms, TitleText, TypingText } from "@/components"
+import { Forms, RiskDiv, TitleText, TypingText } from "@/components"
 
 const Contact: React.FC = () => {
     return (
         <div className="flex justify-center items-center flex-col gap-4 py-4">
-            <TypingText title="| Entre em contato para explorar oportunidades profissionais" />
-            <TitleText title="Construindo parcerias de sucesso para o futuro" />
+            <RiskDiv />
+            <TypingText title="| Entre em contato para explorar oportunidades profissionais" textStyles="w-75" />
+            <TitleText title="Construindo parcerias de sucesso para o futuro"  />
             <Forms />
         </div>
     )
