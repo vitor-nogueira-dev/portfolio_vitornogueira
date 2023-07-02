67f78115232bd8800b7d549a7015e1757bcbb805
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-02 13:29:27 -0300

    fix: removing border and adding padding section contact

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,7 +4,7 @@ import { Forms, TitleText, TypingText } from "@/components"
 
 const Contact: React.FC = () => {
     return (
-        <div className="flex justify-center items-center flex-col gap-4 p-2 border">
+        <div className="flex justify-center items-center flex-col gap-4 py-4">
             <TypingText title="| Entre em contato para explorar oportunidades profissionais" />
             <TitleText title="Construindo parcerias de sucesso para o futuro" />
             <Forms />
