d711184211dc3acea801c39f258b1b86fe1c852a
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-02 13:33:56 -0300

    refactor: refactoring footer to remove contact phrase and change icon layout

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -19,30 +19,25 @@ const Footer: React.FC = () => (
     >
         <div className="footer-gradient" />
         <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-            <div className="flex items-center justify-center flex-wrap gap-3">
-                <h4 className="font-bold text-[16px] text-left text-white w-[600px]">
-                    Interessado em discutir oportunidades profissionais? Estou aberto a novas oportunidades e adoraria colaborar com você! Entre em contato comigo e vamos conversar!
-                </h4>
-                <div className="flex gap-4 flex-row">
-                    {socials.map((social) => (
-                        <RedesSociaisCard
-                            key={social.name}
-                            link={social.link}
-                            name={social.name}
-                            url={social.url}
-                        />
-                    ))}
-                </div>
-            </div>
             <div className="flex flex-col">
-                <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-                <div className="flex items-center justify-center gap-4 z-10">
+                <div className="mb-[10px] h-[2px] bg-white opacity-10" />
+                <div className="flex items-center justify-evenly gap-4 z-10">
                     <Image
                         src="/vn1.png"
                         alt="logo "
                         width={208}
                         height={52}
                     />
+                    <div className="flex gap-4 flex-row">
+                        {socials.map((social) => (
+                            <RedesSociaisCard
+                                key={social.name}
+                                link={social.link}
+                                name={social.name}
+                                url={social.url}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
