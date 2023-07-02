9a478f5e2044c0d141fc5621f59cf832661fa942
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-02 13:04:37 -0300

    feat: rendering component form in section contact

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 
-import { TitleText, TypingText } from "@/components"
+import { Forms, TitleText, TypingText } from "@/components"
 
 const Contact: React.FC = () => {
     return (
         <div className="flex justify-center items-center flex-col gap-4 p-2 border">
             <TypingText title="| Entre em contato para explorar oportunidades profissionais" />
             <TitleText title="Construindo parcerias de sucesso para o futuro" />
+            <Forms />
         </div>
     )
 }
