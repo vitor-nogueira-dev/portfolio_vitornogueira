c7de732ec21e349424ac5ca390a82305a90cfe8d
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-03 14:17:42 -0300

    feat: adding contact id

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,7 +4,7 @@ import { Forms, RiskDiv, TitleText, TypingText } from "@/components"
 
 const Contact: React.FC = () => {
     return (
-        <div className="flex justify-center items-center flex-col gap-4 py-4">
+        <div className="flex justify-center items-center flex-col gap-4 py-4" id="contato">
             <RiskDiv />
             <TypingText title="| Entre em contato para explorar oportunidades profissionais" textStyles="w-75" />
             <TitleText title="Construindo parcerias de sucesso para o futuro"  />
