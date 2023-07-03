27e3cc7445240d13ea4d9ce7e6ead15eeece003a
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-03 14:16:43 -0300

    fix: removing container risk div

--- src/components/Risk.tsx
+++ src/components/Risk.tsx
@@ -3,13 +3,11 @@ import { Col, Container, Row } from "react-bootstrap";
 
 const RiskDiv: React.FC = () => {
     return (
-        <Container>
-            <Row>
-                <Col md={6} className="mx-auto">
-                    <div className="mb-2 h-[0.5px] bg-white opacity-10" />
-                </Col>
-            </Row>
-        </Container>
+        <Row>
+            <Col md={6} className="mx-auto">
+                <div className="mb-2 h-[0.5px] bg-white opacity-10" />
+            </Col>
+        </Row>
     )
 }
 
