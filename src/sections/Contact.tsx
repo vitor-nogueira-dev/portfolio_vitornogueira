abdc487e65c8124a002afda0a116fbb62cdddea0
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-11 09:08:19 -0300

    fix: changing subtitle section contact

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -6,7 +6,7 @@ const Contact: React.FC = () => {
     return (
         <div className="flex justify-center items-center flex-col gap-4 py-4" id="contato">
             <RiskDiv />
-            <TypingText title="| Entre em contato para explorar oportunidades profissionais" textStyles="w-75" />
+            <TypingText title="| Entre em contato comigo" textStyles="w-75" />
             <TitleText title="Construindo parcerias de sucesso para o futuro"  />
             <Forms />
         </div>
